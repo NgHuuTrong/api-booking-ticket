6b886e1097f890447229ea9ba2d44332fc5eb2cf
ngvahiu <vanhieu230303@gmail.com>
2023-09-01 06:56:37 +0700

    add footballers data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -19,20 +19,20 @@ Footballer.belongsTo(Club, { as: 'club', foreignKey: 'club_id' });
 Club.hasMany(Footballer, { as: 'footballers', foreignKey: 'club_id' });
 GroupClub.belongsTo(Club, { as: 'club', foreignKey: 'club_id' });
 Club.hasOne(GroupClub, {
-  as: 'group_club',
-  foreignKey: 'club_id',
+    as: 'group_club',
+    foreignKey: 'club_id',
 });
 Match.belongsTo(Club, { as: 'home_club', foreignKey: 'home_club_id' });
 Club.hasMany(Match, { as: 'matches', foreignKey: 'home_club_id' });
 Match.belongsTo(Club, { as: 'away_club', foreignKey: 'away_club_id' });
 Club.hasMany(Match, { as: 'away_club_matches', foreignKey: 'away_club_id' });
 GroupClub.belongsTo(Group, {
-  as: 'group',
-  foreignKey: 'group_id',
+    as: 'group',
+    foreignKey: 'group_id',
 });
 Group.hasMany(GroupClub, {
-  as: 'group_clubs',
-  foreignKey: 'group_id',
+    as: 'group_clubs',
+    foreignKey: 'group_id',
 });
 Ticket.belongsTo(Match, { as: 'match', foreignKey: 'match_id' });
 Match.hasMany(Ticket, { as: 'tickets', foreignKey: 'match_id' });
@@ -46,45 +46,45 @@ User.hasMany(Ticket, { as: 'tickets', foreignKey: 'user_id' });
 // const models = initModels(sequelize);
 
 sequelize
-  .sync({ force: true })
-  .then(() => {
-    const stadiums = JSON.parse(fs.readFileSync(`${__dirname}/stadium.json`));
-    const clubs = JSON.parse(fs.readFileSync(`${__dirname}/club.json`));
-    const footballers = JSON.parse(
-      fs.readFileSync(`${__dirname}/footballers.json`),
-    );
+    .sync({ force: true })
+    .then(() => {
+        const stadiums = JSON.parse(fs.readFileSync(`${__dirname}/stadium.json`));
+        const clubs = JSON.parse(fs.readFileSync(`${__dirname}/club.json`));
+        const footballers = JSON.parse(
+            fs.readFileSync(`${__dirname}/footballers.json`),
+        );
 
-    const importData = async () => {
-      try {
-        await Stadium.bulkCreate(stadiums);
-        await Club.bulkCreate(clubs);
-        await Footballer.bulkCreate(footballers);
-        console.log('Data imported successfully');
-      } catch (err) {
-        console.log(err);
-      }
-      process.exit();
-    };
+        const importData = async () => {
+            try {
+                await Stadium.bulkCreate(stadiums);
+                await Club.bulkCreate(clubs);
+                await Footballer.bulkCreate(footballers);
+                console.log('Data imported successfully');
+            } catch (err) {
+                console.log(err);
+            }
+            process.exit();
+        };
 
-    const deleteData = async () => {
-      try {
-        // await Tour.deleteMany();
-        // await User.deleteMany();
-        // await Review.deleteMany();
-        console.log('Data deleted successfully');
-      } catch (err) {
-        console.log(err);
-      }
-      process.exit();
-    };
+        const deleteData = async () => {
+            try {
+                // await Tour.deleteMany();
+                // await User.deleteMany();
+                // await Review.deleteMany();
+                console.log('Data deleted successfully');
+            } catch (err) {
+                console.log(err);
+            }
+            process.exit();
+        };
 
-    if (process.argv[2] === '--import') {
-      importData();
-    } else if (process.argv[2] === '--delete') {
-      deleteData();
-    }
-  })
-  .catch((err) => console.log(err));
+        if (process.argv[2] === '--import') {
+            importData();
+        } else if (process.argv[2] === '--delete') {
+            deleteData();
+        }
+    })
+    .catch((err) => console.log(err));
 
 // const models = initModels(sequelize);
 
