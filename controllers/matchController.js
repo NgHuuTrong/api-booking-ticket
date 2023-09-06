c0378a78c928ca62f37e162f1cfbb0279d84d3f2
Nguyen Huu Trong <trongnh2003@gmail.com>
2023-09-06 09:15:36 +0700

    match and group routes

---
+++ controllers/matchController.js
@@ -0,0 +1,36 @@
+const db = require('../utils/database');
+const factory = require('./handleFactory');
+
+exports.getALlMatch = factory.getAll(db.matches, [
+  {
+    model: db.clubs,
+    as: 'home_club',
+    foreignKey: 'home_club_id',
+    attributes: ['name', 'logo'],
+  },
+  {
+    model: db.clubs,
+    as: 'away_club',
+    foreignKey: 'away_club_id',
+    attributes: ['name', 'logo'],
+  },
+]);
+
+exports.getMatch = factory.getOne(db.matches, [
+  {
+    model: db.clubs,
+    as: 'home_club',
+    foreignKey: 'home_club_id',
+  },
+  {
+    model: db.clubs,
+    as: 'away_club',
+    foreignKey: 'away_club_id',
+  },
+]);
+
+exports.createMatch = factory.createOne(db.matches);
+
+exports.updateMatch = factory.updateOne(db.matches);
+
+exports.deleteMatch = factory.deleteOne(db.matches);
